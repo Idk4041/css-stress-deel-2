32301c9e6bc79f658bf25e80a3619cb55755b58b
Luca <lucadebruin@gmail.com>
2026-02-19 09:42:18 +0100

    De html van assortiment weg gehaald en in de javascript array gezet

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
 
 const plantenAssortiment = [
     {
-        naam: "iomiomi",
-        prijs: 19.95,
-        afbeelding: "images/Nora'sFloraLogo.png",
-        beschrijving: "Populaire gatenplant."
+        naam: "Zonnebloemen",
+        prijs: 5.00,
+        afbeelding: "images/pexels-pixabay-54267.jpg",
+        beschrijving: "Zonnebloemen."
     },
     {
-        naam: "iomiomi",
-        prijs: 19.95,
-        afbeelding: "images/Nora'sFloraLogo.png",
-        beschrijving: "Populaire gatenplant."
+        naam: "Rode tulpen",
+        prijs: 7.68,
+        afbeelding: "images/pexels-valeriya-1961778.jpg",
+        beschrijving: "Populaire rode tulpen."
     },
     {
-        naam: "iomiomi",
-        prijs: 19.95,
-        afbeelding: "images/Nora'sFloraLogo.png",
-        beschrijving: "Populaire gatenplant."
+        naam: "Cactus",
+        prijs: 2.00,
+        afbeelding: "images/pexels-scottwebb-403571.jpg",
+        beschrijving: "Populaire cactus."
     },
     {
-        naam: "iomiomi",
-        prijs: 19.95,
-        afbeelding: "images/Nora'sFloraLogo.png",
-        beschrijving: "Populaire gatenplant."
+        naam: "Roze bouquet",
+        prijs: 19.60,
+        afbeelding: "images/pexels-secret-garden-333350-931176.jpg",
+        beschrijving: "Speciale roze bouquet."
     },
+    {
+        naam: "Lente bouquet",
+        prijs: 20.00,
+        afbeelding: "images/pexels-valeriya-1484657.jpg",
+        beschrijving: "Speciale lente bouquet."
+    },
+    {
+        naam: "Diverse vet planten",
+        prijs: 5.00,
+        afbeelding: "images/pexels-maureen-piecesphotography-1207978.jpg",
+        beschrijving: "Populaire vet planten."
+    },
+    {
+        naam: "Trouwdag bouquet bundel",
+        prijs: 25.00,
+        afbeelding: "images/pexels-43381756-7462761.jpg",
+        beschrijving: "Speciaal samengestelde trouwdag bouquet bundel"
+    },
+    {
+        naam: "Diverse lenten bloemen",
+        prijs: 7.50,
+        afbeelding: "images/pexels-jos-van-ouwerkerk-377363-1075960.jpg",
+        beschrijving: "Diverse lenten bloemen"
+    },
+    {
+        naam: "Paarse Allium bloemen",
+        prijs: 4.00,
+        afbeelding: "images/pexels-mikebirdy-109828.jpg",
+        beschrijving: "Paarse Allium bloemen"
+    }
 
 ];
 
 function toonPlanten() {
-    const container = document.getElementById('planten-container');
+    const container = document.getElementById('producten');
     let htmlContent = '';
 
 
